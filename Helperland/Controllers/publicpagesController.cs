7a8c5a5d68c2bf0444ebe201c21d098a85ee29e4
Krishnashah929 <69577884+Krishnashah929@users.noreply.github.com>
2022-01-28 17:08:56 +0530

    Add project files.

---
+++ Helperland/Controllers/publicpagesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class publicpagesController : Controller
+    {
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+        public IActionResult CONTACT()
+        {
+            return View();
+        }
+        public IActionResult ABOUT()
+        {
+            return View();
+        }
+        public IActionResult PRICES()
+        {
+            return View();
+        }
+        public IActionResult CREATE_ACCOUNT()
+        {
+            return View();
+        }
+    }
+}
---
+++ Helperland/wwwroot/js/site.js
@@ -0,0 +1,42 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*<!--Script for side bar-->*/
+$(document).ready(function () {
+    $('#dismiss, .overlay').on('click', function () {
+        // hide sidebar
+        $('#sidebar').removeClass('active');
+        // hide overlay
+        $('.overlay').removeClass('active');
+        $('.model').removeClass('active');
+    });
+    $('#sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('#sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
+/* <!--Script for side bar-->*/
+
+/*<!--cookie 's js'--> */
+function myFunction() {
+    var x = document.getElementById("privacy-policy-home");
+    x.style.display = "none";
+}
+/*<!--cookie 's js'--> */
+
+
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").style.backgroundColor = "rgba(19,19,19,0.8)";
+    }
+}
+
+
+
+
